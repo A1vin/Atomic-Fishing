b1c21b51f04986ef1ab54d1733aec8820a984413
Pekaaw <pekaaw@gmail.com>
2012-11-10 03:05:32 +0100

    pk03 canvas med storleik og innhald

--- atomicFishing.js
+++ atomicFishing.js
@@ -1,4 +1,11 @@
 function atomicFishing()
 {
-	alert( "hei" );
+	var canvas = document.getElementById("gameFrame");
+	canvas.width = 800;
+	canvas.height = 600;
+	canvas.style.border = "5px solid black";
+	var context = canvas.getContext("2d");
+	var img_background = new Image();
+	img_background.src = "background.jpg";
+	context.drawImage( img_background, 0, 0);
 }
